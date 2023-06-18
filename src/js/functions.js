769ff83d9050bff367a89bbfc1f3619165fd72f2
Sara Piroozvand <sarapiroozvand@gmail.com>
2023-06-18 23:45:10 +0330

    commited functions

---
+++ src/js/functions.js
@@ -0,0 +1,48 @@
+/*Is "else" required? */
+// let age = prompt("What is your age?", 18);
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   }
+//   return confirm('Did parents allow you?');
+// }
+// checkAge();
+//YES it worked
+
+/*********************************************************************************** */
+
+/*Rewrite the function using '?' or '||' */
+// let age = prompt("What is your age?");
+// function checkAge(age) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return confirm('Did parents allow you?');
+//     }
+// }
+// with ?
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Did parents allow you?');
+// }
+// with ||
+// function checkAge(age) {
+//     return (age > 18) || confirm('Did parents allow you?');
+// }
+
+// checkAge(age);
+
+
+/*********************************************************************************** */
+
+/* Function min(a, b) */
+// let a = prompt("a");
+// let b = prompt("b");
+// let min = function (a,b) {
+//     if (a < b) {
+//         alert(a) ;
+//     } else {
+//         alert (b);
+//     }
+// }
+// min(a,b);
