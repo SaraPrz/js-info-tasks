ac758af6cd2d30f6bebe65a8b07123f687239a29
Sara Piroozvand <sarapiroozvand@gmail.com>
2023-06-18 18:07:13 +0330

    commited loops.js

---
+++ src/js/loops.js
@@ -0,0 +1,22 @@
+/* Rewrite the code changing the for loop to while without altering its behavior (the output should stay same). */
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+// }
+/* while : */
+// let i = 0;
+// while (i < 3) {
+//     alert (`number ${i}!`)
+//     i ++;
+// }
+
+/* Output even numbers in the loop */
+//---- my bad solution :D -----
+// for (let i = 0 ; i < 11 ; i += 2 ) {
+//     alert (i);
+// }
+
+// for (let i = 2 ; i <= 10 ; i += 2 ) {
+//     if (i % 2 == 0) {
+//         alert( i );
+//     }
+// }
